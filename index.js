4ae91a9a1cdeb076d714f75130b3e18d74499038
HanSolShin <wkdqhrh010@naver.com>
2023-01-08 17:33:03 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://HanSol:Gksthf717!@johnahn-course.mzo2q0l.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+
+
+app.get('/', (req, res) => res.send('Hello World!!!!!!!!!!!!!!!!!!!!!'))
+
+app.listen(port, () => console.log('Example app listening on port ${port}!'))
